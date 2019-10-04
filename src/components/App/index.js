740311d0bce066d194332451c14d3638603ff9c7
Jeremy Yandell <jereyand@gmail.com>
2019-10-04 15:45:45 -0700

    move app.js

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () =>  {
+    return(
+        <div>
+            Hello, let guitar guide show you the way
+        </div>
+    )
+}
+
+export default App;
