7069c32a995fbb365642840ccaeb65f59672db37
Jeremy Yandell <jereyand@gmail.com>
2019-10-05 13:59:02 -0700

    add function to build array of number of strings and frets

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
@@ -14,6 +14,21 @@ const App = () =>  {
     const closeModal = () =>  {
         setShowModal(false);
     }
+    //using this to build arrays or strings and frets to contain names of notes
+    const buildFretboard = (strings, frets) =>  {
+        let totalStrings = [];
+        let totalFrets = [];
+        for (let i = 1; i <= frets; i++) {
+            totalFrets.push('x');
+        }
+        for (let i = 1; i <= strings; i++) {
+            totalStrings.push('o');
+        }
+    }
+    useEffect(() =>  {
+        buildFretboard(numberOfStrings, numberOfFrets);
+    }, [numberOfFrets, numberOfStrings]);
+
     return(
         <div>
             Hello, let guitar guide show you the way
