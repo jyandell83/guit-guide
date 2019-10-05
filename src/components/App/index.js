e37ee8dce9cacea6614db6ffdea025d1baaa4612
Jeremy Yandell <jereyand@gmail.com>
2019-10-05 14:53:27 -0700

    displaying each strings notes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,26 +4,27 @@ import React, { useState, useEffect } from 'react';
 
 import WelcomeModal from '../WelcomeModal'
 
-
+const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 const App = () =>  {
     const [showModal, setShowModal] = useState(true);
     const [numberOfStrings, setNumberOfStrings] = useState(6);
     const [numberOfFrets, setNumberOfFrets] = useState(24);
+    const [fretboard, setFretboard] = useState([]);
     //using this to close welcome model when people submit the form
     const closeModal = () =>  {
         setShowModal(false);
     }
     //using this to build arrays or strings and frets to contain names of notes
     const buildFretboard = (strings, frets) =>  {
-        let totalStrings = [];
-        let totalFrets = [];
-        for (let i = 1; i <= frets; i++) {
-            totalFrets.push('x');
+        let fretArray = [];
+        let openNote = 'E';
+        
+        for(let i = 1; i <= strings; i++) {
+            let guitarString = notes.slice(notes.indexOf(openNote)).concat(notes, notes);
+            fretArray.push(guitarString);
         }
-        for (let i = 1; i <= strings; i++) {
-            totalStrings.push('o');
-        }
+        setFretboard(fretArray);
     }
     useEffect(() =>  {
         buildFretboard(numberOfStrings, numberOfFrets);
@@ -32,7 +33,10 @@ const App = () =>  {
     return(
         <div>
             Hello, let guitar guide show you the way
-            {numberOfStrings},{numberOfFrets}
+            {numberOfStrings},{numberOfFrets}<br />
+            {
+                 fretboard.map((elem,i) => <div key={i}>{elem}</div>)
+            }
             {
                 showModal ? <WelcomeModal closeModal ={closeModal} setNumberOfFrets={setNumberOfFrets} setNumberOfStrings={setNumberOfStrings} numberOfFrets ={numberOfFrets} numberOfStrings={numberOfStrings}/> : null
             }
