f4a7d29d2589339eb680f4613c30546b0f585d24
Jeremy Yandell <jereyand@gmail.com>
2019-10-06 14:31:42 -0700

    add tuners for each string

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 import WelcomeModal from '../WelcomeModal'
 
 const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+const openNotes = ['E', 'A', 'D', 'G', 'B', 'E'];
 
 const App = () =>  {
     const [showModal, setShowModal] = useState(true);
@@ -18,9 +19,10 @@ const App = () =>  {
     //using this to build arrays or strings and frets to contain names of notes
     const buildFretboard = (strings, frets) =>  {
         let fretArray = [];
-        let openNote = 'E';
         
-        for(let i = 1; i <= strings; i++) {
+        
+        for(let i = 0; i < strings; i++) {
+            let openNote = openNotes[i];
             let guitarString = notes.slice(notes.indexOf(openNote)).concat(notes, notes);
             fretArray.push(guitarString);
         }
@@ -29,16 +31,35 @@ const App = () =>  {
     useEffect(() =>  {
         buildFretboard(numberOfStrings, numberOfFrets);
     }, [numberOfFrets, numberOfStrings]);
-
+    console.log(fretboard)
     return(
         <div>
             Hello, let guitar guide show you the way
             {numberOfStrings},{numberOfFrets}<br />
             {
-                 fretboard.map((elem,i) => <div key={i}>{elem}</div>)
+                //these are the tuners for lack of a better word
+                 fretboard.map((elem,i) => 
+                    <div key={i}>
+                        <select onChange={(e => console.log(e.target.value))}>
+                            {
+                                notes.map((note, index) =>  
+                                    <option key={index}>{note}</option>
+                                )
+                            }
+                        </select>
+                        <div>{elem}</div>
+                    </div>)
             }
             {
-                showModal ? <WelcomeModal closeModal ={closeModal} setNumberOfFrets={setNumberOfFrets} setNumberOfStrings={setNumberOfStrings} numberOfFrets ={numberOfFrets} numberOfStrings={numberOfStrings}/> : null
+                showModal ? <WelcomeModal 
+                                closeModal ={closeModal} 
+                                setNumberOfFrets={setNumberOfFrets} 
+                                setNumberOfStrings={setNumberOfStrings} 
+                                numberOfFrets ={numberOfFrets} 
+                                numberOfStrings={numberOfStrings}
+                                /> 
+                            : 
+                            null
             }
         </div>
     )
