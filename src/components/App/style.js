43a8729720ae2aa3469b2fa77302b5e9f0835fec
Jeremy Yandell <jereyand@gmail.com>
2019-10-12 17:21:04 -0700

    change style

--- src/components/App/style.js
+++ src/components/App/style.js
@@ -3,15 +3,12 @@ import styled from 'styled-components';
 
 export const FretNote = styled.div`
     display: inline;
-    width: 50%;
-    padding: 5px;
     
 `
 export const FretContainer = styled.div`
     width: 40px;
     border: 1px solid black;
     text-align: center;
-    padding: 5px 7px;
 `
 
 export const GuitarString = styled.div`
