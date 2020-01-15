1be222fb8bfd7e845241f5676859b717099938ef
Jeremy Yandell <jereyand@gmail.com>
2020-01-14 22:28:50 -0800

    start grid for dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,26 +6,30 @@ import {GuitarString} from './style';
 const Dashboard = ({setScale, setRoot, notes, fretboard, openNotes, setOpenNotes, scaleNotes, root}) =>  {
     return(
         <div className="dashboard">
-            <h3>DASHBOARD</h3>
-            <div className="margin-top-small">
-                ROOT:
-                <select onChange={(e) => setRoot(e.target.value)}>
-                    <option disabled selected>- Change Root -</option>
-                    {
-                        notes.map((note, index) =>  
-                            <option key={index}>{note}</option>
-                        )
-                    }
-                </select>
-                SCALE:
-                <select onChange={(e) => setScale(e.target.value)}>
-                    <option disabled selected>- Change Scale -</option>
-                    <option>Major</option>
-                    <option>Minor</option>
-                    <option>Major Pentatonic</option>
-                    <option>Minor Pentatonic</option>
-                </select><br />
-            </div>
+            <h3 className="dashboard__header">DASHBOARD</h3>
+                <div>
+                    ROOT:
+                    <select onChange={(e) => setRoot(e.target.value)}>
+                        <option disabled selected>- Change Root -</option>
+                        {
+                            notes.map((note, index) =>  
+                                <option key={index}>{note}</option>
+                            )
+                        }
+                    </select>
+                </div>
+                <div>
+                    SCALE:
+                    <select onChange={(e) => setScale(e.target.value)}>
+                        <option disabled selected>- Change Scale -</option>
+                        <option>Major</option>
+                        <option>Minor</option>
+                        <option>Major Pentatonic</option>
+                        <option>Minor Pentatonic</option>
+                    </select>
+
+                </div>
+           
             {
                 //these are the tuners for lack of a better word
                  fretboard.map((elem,i) => 
