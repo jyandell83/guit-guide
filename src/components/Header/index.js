8458ff1d9b30d0b69ee7756074cf9b9cc5688f2a
Jeremy Yandell <jereyand@gmail.com>
2020-01-15 09:52:23 -0800

    change the font

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ import React from 'react';
 const Header = ({root, scale}) =>  {
     return(
         <div>
-            <h1 className="margin-top-small">
-                Hello, let guitar guide show you the way
+            <h1 className="margin-top-small primary-header">
+                The Guit Guide
             </h1>
             <h2 className="margin-top-small">
                 {root} - {scale}
